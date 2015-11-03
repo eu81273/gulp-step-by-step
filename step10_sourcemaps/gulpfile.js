1c895135b2991c3b74c785457a42b231111723d8
AHN JAE-HA <eu81273@gmail.com>
2015-11-03 18:14:14 +0900

    step 10 sourcemaps 추가

---
+++ step10_sourcemaps/gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var minifyhtml = require('gulp-minify-html');
+var browserSync = require('browser-sync').create();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps  = require('gulp-sourcemaps');
+
+//dist 폴더를 기준으로 웹서버 실행
+gulp.task('server', ['uglify', 'minifycss', 'minifyhtml'], function () {
+    return browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+});
+
+//HTML 파일을 minify
+gulp.task('minifyhtml', function () {
+    return gulp.src('src/**/*.html') //src 폴더 아래의 모든 html 파일을
+        .pipe(minifyhtml()) //minify 해서
+        .pipe(gulp.dest('dist')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//자바스크립트 파일을 browserify로 번들링
+gulp.task('uglify', function () {
+    return browserify({entries: ['src/js/main.js'], debug: true})
+        .bundle() //browserify로 번들링
+        .pipe(source('main.js')) //vinyl object 로 변환
+        .pipe(buffer()) //buffered vinyl object 로 변환
+        .pipe(sourcemaps.init({loadMaps: true, debug: true})) //소스맵 생성 준비
+        .pipe(uglify()) //minify 해서
+        .pipe(sourcemaps.write('./')) //생성된 소스맵을 스트림에 추가
+        .pipe(gulp.dest('dist/js')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//CSS 파일을 minify
+gulp.task('minifycss', function () {
+    return gulp.src('src/**/*.css') //src 폴더 아래의 모든 css 파일을
+        .pipe(sourcemaps.init({loadMaps: true, debug: true})) //소스맵 생성 준비
+        .pipe(concat('main.css')) //병합하고
+        .pipe(minifycss()) //minify 해서
+        .pipe(sourcemaps.write('./')) //생성된 소스맵을 스트림에 추가
+        .pipe(gulp.dest('dist/css')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//파일 변경 감지
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.js', ['uglify']);
+    gulp.watch('src/**/*.css', ['minifycss']);
+    gulp.watch('src/**/*.html', ['minifyhtml']);
+});
+
+//gulp를 실행하면 default 로 minifycss task를 실행
+gulp.task('default', ['server', 'watch']);
---
+++ step10_sourcemaps/src/js/modules/module1.js
@@ -0,0 +1,6 @@
+//dummy hello world function
+function hello1 () {
+    console.log('hello world! - 1');
+}
+
+module.exports = hello1;
